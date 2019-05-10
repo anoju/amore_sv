365e1b0ae1cc48cea79e58af08aadb8595110541
zun5761 <zun5761@gmail.com>
2019-05-10 13:38:38 +0900

    버전업

--- html/resource/js/ui-front.js
+++ html/resource/js/ui-front.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	svPopupUI();
 	scrollItem();
+	winScrollUI();
 	if($('.ui-roll-number').length > 0)rollingNumberInit();
 	if($('.ui-split-text').length > 0)splitText('.ui-split-text');
 
@@ -9,15 +10,43 @@ $(document).ready(function(){
 	});
 });
 
-//설문항목 이미지타입 체크
+//설문항목 이미지, iframe 타입 체크
 var svItemImgType = function(){
-	if($('.sv_check_item').size() > 0){
-		var $items = $.find('.sv_check_item');
-		$.each($items, function(){
+	var $items = $('.sv_check_item');
+	if($items.length > 0){
+		$items.each(function(){
 			var $size = $(this).find('img').size();
 			if($size > 0){
-				$(this).closest('.sv_check_item_wrap').addClass('type3');
+				var $wrap = $(this).closest('.sv_check_item_wrap'),
+					$html = '<div class="sv_view_util"><a href="#" class="btn_view_util list on">리스트형</a><a href="#" class="btn_view_util gal">갤러리형</a></div>';
+				if(!$(this).find('img').hasClass('sv_img'))$(this).find('img').wrap('<div class="sv_img"></div>');
+				if(!$wrap.hasClass('type3'))$wrap.addClass('type3');
+				if($wrap.prev('.sv_view_util').length == 0)$wrap.before($html);
 			}
+		});
+		var $imgViewType = localStorage.getItem('imgViewType');
+		if($imgViewType == 'gallery' && $('.sv_check_item_wrap').length == 1){
+			$('.btn_view_util.gal').addClass('on').siblings().removeClass('on');
+			$('.sv_check_item_wrap').addClass('gallery');
+		}
+	}
+	$(document).on('click','.btn_view_util',function(e){
+		e.preventDefault();
+		var $wrap = $(this).parent().next('.sv_check_item_wrap');
+		if($(this).hasClass('gal')){
+			$wrap.addClass('gallery');
+			if($('.sv_check_item_wrap').length == 1)localStorage.setItem('imgViewType','gallery');
+		}else{
+			$wrap.removeClass('gallery');
+			localStorage.removeItem('imgViewType');
+		}
+		$(this).addClass('on').siblings().removeClass('on');
+	});
+
+	var $iframes = $('.sv_survey iframe');
+	if($iframes.length > 0){
+		$iframes.each(function(){
+			if(!$(this).parent().hasClass('iframe_veideo'))$(this).wrap('<div class="iframe_veideo"></div>');
 		});
 	}
 };
@@ -39,6 +68,7 @@ var svPopOpen = function(tar){
 	if($(tar).length < 1) return console.log('해당팝업없음');
 	$('body').addClass('scroll_lock');	
 	$(tar).addClass('open');
+	if($('.confetti').length > 0) complateEffect('.confetti');	//인터렉션 함수;
 };
 var svPopClose = function(tar){
 	$(tar).fadeOut(500,function(){
@@ -53,13 +83,16 @@ var randomNumber = function(min,max,point){
 };
 
 //완료 인터렉션
-var complateEffect = function(wrap,length){
-	if(length > 20)length = 20;
+var complateEffect = function(wrap){
 	var $wrap = $(wrap),
-		$itemLength = length,
+		$itemLength = 10,			//20 넘지 않게
 		rdClass, rdLeft, rdTop, rdDelay,rdDirection, rdSpeed, 
 		$html = '',
 		rdLeftAry = [];
+
+	if($wrap.hasClass('type1') || $wrap.hasClass('type3'))$itemLength = 15;
+	console.log($itemLength);
+
 	for(var i = 0; i < $itemLength;i++){
 		rdClass = randomNumber(1,3,0);
 		rdSize = randomNumber(1,3,0);
@@ -69,7 +102,7 @@ var complateEffect = function(wrap,length){
 		rdDelay = randomNumber(0,10,0) * 200;
 		//rdDelay = (i%10) * 200;
 		rdDirection = randomNumber(1,2,0);
-		rdSpeed = randomNumber(25,60,0) * 100;
+		rdSpeed = randomNumber(25,50,0) * 100;
 		
 		if(rdLeftAry.indexOf(rdLeft) >= 0){		//left 랜덤값 겹치지않게
 			i--;
@@ -78,47 +111,77 @@ var complateEffect = function(wrap,length){
 			if($wrap.hasClass('type1')){
 				//꽃가루(2가지 모션, 3가지 컬러, 3가지 사이즈, 6가지 모양)
 				rdClass = randomNumber(1,6,0);
-				$html = '<span class="item item'+rdClass+' color'+rdColor+' size'+rdSize+'" style="left:'+rdLeft+'%;animation:confettiSwing'+rdDirection+' '+(rdSpeed/2)+'ms infinite '+rdDelay+'ms, confettiDrop '+rdSpeed+'ms infinite ease-out '+rdDelay+'ms;"><span></span></span>';
+				$html = '<span class="item item'+rdClass+' color'+rdColor+' size'+rdSize+'" style="left:'+rdLeft+'%;';
+					$html += '-webkit-animation:confettiSwing'+rdDirection+' '+(rdSpeed/2)+'ms infinite '+rdDelay+'ms, confettiDrop '+rdSpeed+'ms infinite ease-out '+rdDelay+'ms;';
+					$html += 'animation:confettiSwing'+rdDirection+' '+(rdSpeed/2)+'ms infinite '+rdDelay+'ms, confettiDrop '+rdSpeed+'ms infinite ease-out '+rdDelay+'ms;';
+				$html += '"><span></span></span>';
 			}else if($wrap.hasClass('type2')){
 				//코인(1가지 모양, 3가지 사이즈)
 				rdSpeed = randomNumber(10,15,0) * 100;	//속도조절
-				$html = '<span class="item size'+rdSize+'" style="left:'+rdLeft+'%;animation:confettiCoin '+rdSpeed+'ms infinite linear '+rdDelay+'ms;"></span>';
+				$html = '<span class="item size'+rdSize+'" style="left:'+rdLeft+'%;';
+					$html += '-webkit-animation:confettiCoin '+rdSpeed+'ms linear '+rdDelay+'ms;';
+					$html += 'animation:confettiCoin '+rdSpeed+'ms linear '+rdDelay+'ms;';
+				$html += '"></span>';
 			}else if($wrap.hasClass('type3')){
 				//깜빡임(3가지 모양, 3가지 사이즈, 3가지 컬러)
 				rdSpeed = randomNumber(5,15,0) * 100;	//속도조절
 				rdDelay = randomNumber(0,5,0) * 200;	//딜레이조절
-				$html = '<span class="item item'+rdClass+' color'+rdColor+'" style="left:'+rdLeft+'%;top:'+rdTop+'%;animation:confettiFlash '+rdSpeed+'ms infinite '+rdDelay+'ms"></span>';
+				$html = '<span class="item item'+rdClass+' color'+rdColor+'" style="left:'+rdLeft+'%;top:'+rdTop+'%;';
+					$html += '-webkit-animation:confettiFlash '+rdSpeed+'ms infinite '+rdDelay+'ms;';
+					$html += 'animation:confettiFlash '+rdSpeed+'ms infinite '+rdDelay+'ms;';
+				$html += '"></span>';
 			}else if($wrap.hasClass('type4')){
 				//풍선(3가지 모양, 3가지 사이즈)
-				$html = '<span class="item color'+rdColor+' size'+rdSize+'" style="left:'+rdLeft+'%;animation:confettiBalloon'+rdDirection+' '+(rdSpeed/2)+'ms infinite '+rdDelay+'ms, confettiUp '+rdSpeed+'ms infinite ease-out '+rdDelay+'ms;"></span>';
+				$html = '<span class="item color'+rdColor+' size'+rdSize+'" style="left:'+rdLeft+'%;';
+					$html += '-webkit-animation:confettiBalloon'+rdDirection+' '+(rdSpeed/2)+'ms infinite '+rdDelay+'ms, confettiUp '+rdSpeed+'ms infinite ease-out '+rdDelay+'ms;';
+					$html += 'animation:confettiBalloon'+rdDirection+' '+(rdSpeed/2)+'ms infinite '+rdDelay+'ms, confettiUp '+rdSpeed+'ms infinite ease-out '+rdDelay+'ms;';
+				$html += '"></span>';
 			}else if($wrap.hasClass('type5')){
 				//불꽃(3가지 모양)
 				rdTop = randomNumber(0,8,0) * 5;		//top값 조정
 				rdSpeed = randomNumber(15,25,0) * 100;	//속도조절
-				$html = '<span class="item color'+rdColor+'" style="left:'+rdLeft+'%;top:'+rdTop+'%;">';
-				$html += '<span class="firework" style="animation:confettiFirework '+rdSpeed+'ms infinite '+rdDelay+'ms"></span>';
-				$html += '<span class="fire_arr"><i style="animation:confettiFireArr '+rdSpeed+'ms infinite '+rdDelay+'ms"></i></span>';
+				$html = '<span class="item color'+rdColor+' size'+rdSize+'" style="left:'+rdLeft+'%;top:'+rdTop+'%;">';
+					$html += '<span class="firework" style="';
+						$html += '-webkit-animation:confettiFirework '+rdSpeed+'ms infinite '+rdDelay+'ms;';
+						$html += 'animation:confettiFirework '+rdSpeed+'ms infinite '+rdDelay+'ms;';
+					$html += '"></span>';
+					$html += '<span class="fire_arr"><i style="';
+						$html += '-webkit-animation:confettiFireArr '+rdSpeed+'ms infinite '+rdDelay+'ms;';
+						$html += 'animation:confettiFireArr '+rdSpeed+'ms infinite '+rdDelay+'ms;';
+					$html += '"></i></span>';
 				$html += '</span>';
 			}else if($wrap.hasClass('type6')){
 				//하트(2가지 모양, 3가지 각도)
 				rdColor = (i%2) + 1;  					//하트이미지 2종류
 				rdSpeed = randomNumber(5,15,0) * 100;	//속도조절
 				rdDelay = randomNumber(0,5,0) * 200;	//딜레이조절
-				$html = '<span class="item item'+rdClass+' color'+rdColor+'" style="left:'+rdLeft+'%;top:'+rdTop+'%;animation:confettiFlash '+rdSpeed+'ms infinite '+rdDelay+'ms"></span>';
+				$html = '<span class="item item'+rdClass+' color'+rdColor+'" style="left:'+rdLeft+'%;top:'+rdTop+'%;';
+					$html += '-webkit-animation:confettiFlash '+rdSpeed+'ms infinite '+rdDelay+'ms;';
+					$html += 'animation:confettiFlash '+rdSpeed+'ms infinite '+rdDelay+'ms;';
+				$html += '"></span>';
 			}else if($wrap.hasClass('type7')){
 				//별빛(1가지 모양, 4가지 크기)
 				rdTop = randomNumber(0,10,0) * 5;		//top값 조정
 				rdSize = (i%4) + 1;						//크기 4가지
 				rdSpeed = randomNumber(10,15,0) * 100;	//속도조절
 				rdDelay = randomNumber(0,5,0) * 200;	//딜레이조절
-				$html = '<span class="item  size'+rdSize+'" style="left:'+rdLeft+'%;top:'+rdTop+'%;animation:confettiFlash '+rdSpeed+'ms infinite '+rdDelay+'ms"></span>';
+				$html = '<span class="item  size'+rdSize+'" style="left:'+rdLeft+'%;top:'+rdTop+'%;';
+					$html += '-webkit-animation:confettiFlash '+rdSpeed+'ms infinite '+rdDelay+'ms;';
+					$html += 'animation:confettiFlash '+rdSpeed+'ms infinite '+rdDelay+'ms;';
+				$html += '"></span>';
 			}else if($wrap.hasClass('type8')){
 				//불꽃2(3가지 모양)
 				rdTop = randomNumber(0,14,0) * 5;		//top값 조정
 				rdSpeed = randomNumber(15,25,0) * 100;	//속도조절
-				$html = '<span class="item color'+rdColor+'" style="left:'+rdLeft+'%;top:'+rdTop+'%;">';
-				$html += '<span class="dot" style="animation:confettiFireworkDot '+rdSpeed+'ms infinite '+rdDelay+'ms"></span>';
-				$html += '<span class="firework" style="animation:confettiFirework2 '+rdSpeed+'ms infinite '+rdDelay+'ms"></span>';
+				$html = '<span class="item color'+rdColor+' size'+rdSize+'" style="left:'+rdLeft+'%;top:'+rdTop+'%;">';
+					$html += '<span class="dot" style="';
+						$html += '-webkit-animation:confettiFireworkDot '+rdSpeed+'ms infinite '+rdDelay+'ms;';
+						$html += 'animation:confettiFireworkDot '+rdSpeed+'ms infinite '+rdDelay+'ms;';
+					$html += '"></span>';
+					$html += '<span class="firework" style="';
+						$html += '-webkit-animation:confettiFirework2 '+rdSpeed+'ms infinite '+rdDelay+'ms;';
+						$html += 'animation:confettiFirework2 '+rdSpeed+'ms infinite '+rdDelay+'ms;';
+					$html += '"></span>';
 				$html += '</span>';
 			}else{
 				console.log('인터렉션 타입 클래스를 적용해주세요');
@@ -315,5 +378,34 @@ var splitText = function(tar){
 		}
 		$this.html($html).removeAttr('style');
 		if($style)$this.attr('style',$style);
+	});
+};
+
+var winScrollUI = function(){
+	var prevScrollTop = $(window).scrollTop(),
+		nowScrollTop = $(window).scrollTop(),
+		scrollTimeout = '',
+		timer = 100,
+		gap = 5;
+
+	$(window).on('scroll touchmove', function (){
+		//clearTimeout(scrollTimeout);
+		//scrollTimeout = setTimeout(function(){
+			nowScrollTop = $(window).scrollTop();
+			if (nowScrollTop > (prevScrollTop + gap)) {
+				//console.log('down');
+				if($('.myranking').length > 0)$('.myranking').removeClass('on');
+				
+			} else if (nowScrollTop < (prevScrollTop - gap)){
+				//console.log('up');
+				if($('.myranking').length > 0)$('.myranking').addClass('on');
+			}
+			prevScrollTop = nowScrollTop;
+		//},timer);
+	});
+
+	$(window).on('load', function (){
+		nowScrollTop = $(window).scrollTop();
+		if($('.myranking').length > 0 && nowScrollTop > gap)$('.myranking').addClass('on');
 	});
 };
